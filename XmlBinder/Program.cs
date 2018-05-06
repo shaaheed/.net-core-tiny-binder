329c5493f9c8e225fc6e5cc7e29857e20f3c57d4
Shahidul Islam <shahidcse6@gmail.com>
2018-05-06 20:01:51 +0600

    Add project files.

---
+++ XmlBinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XmlBinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
